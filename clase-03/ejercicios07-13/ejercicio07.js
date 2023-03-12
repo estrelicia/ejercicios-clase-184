f3db0bdad8290ecaaacfcb2e1e8408184b9ebe2e
Ale N <ale@watsabi.com>
2023-03-12 11:29:49 -0300

    Finalizados los ejercicios 01-03 de la clase 05

--- clase-03/ejercicios07-13/ejercicio07.js
+++ clase-03/ejercicios07-13/ejercicio07.js
@@ -7,14 +7,18 @@ function esPalindromo(palindromo) {
     fraseLadoA = parametro1.join('');
     console.log('Frase A ' + fraseLadoA);
      
+    // Se convierte fraseLadoA en array y se asigna a fraseLadoB que a su vez, es un array que se invierte, y luego se convierte en string.
     fraseLadoB = fraseLadoA.split('');
-    fraseLadoB = fraseLadoB.reverse();
+    fraseLadoB = parametro1.reverse();
     fraseLadoB = fraseLadoB.join('');
     console.log('Frase B ' + fraseLadoB);
 
+    /* Un palindromo se puede leer de ambos lados, o sea que la frase A (conjunto de caracteres sin espacios) 
+    y la frase B (ese mismo conjunto de caracteres sin espacios pero invertido) 
+    deberian ser iguales en caso de ser palindromo. 
+    */
     if (fraseLadoA.toLowerCase() == fraseLadoB.toLowerCase()) { console.log('Es un palindromo') }
     else { console.log('No es un palindromo') }
-    
 }
 
 esPalindromo(palindromo);
